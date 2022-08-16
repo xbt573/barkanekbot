38f789e85cd5577e0884bafebf7641aacf0328fd
xbt573 <alunev5@yandex.ru>
2022-08-16 12:51:11 +0300

    fix channel gathering

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const botToken: string = config.get('bot.token');
 const apiId: number = config.get('bot.apiId');
 const apiHash: string = config.get('bot.apiHash');
 const stringSession: string = config.get('bot.stringSession') || '';
+const channels: Array<string> = config.get('bot.channels') || ['anekdotcollection'];
 
 // eslint-disable-next-line
-const bot = new Bot(botToken, apiId, apiHash, stringSession, ['anekdotcollection']);
+const bot = new Bot(botToken, apiId, apiHash, stringSession, channels);
